b04b49d6f9d2547b5d3b605babfde3b4795a9884
AlbertoDM2004 <albertodimi2004@gmail.com>
2026-09-25 14:38:03 +0200

    12

---
+++ Proyectos Programacion/R3/R3E06_AlbertoDM/R3E06V1_AlbertoDM/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace R3E06V1_AlbertoDM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONSTANTES
+
+            // VARIABLES
+            int ancho, alto;
+            int superficieEntera;
+            string cadenaAuxiliar;
+            float superficieDecimal;
+
+            // ENTRADA
+            // 1.- Captar ancho
+            Console.Write("Introduzca el ancho del triangulo: ");
+            cadenaAuxiliar = Console.ReadLine();
+            ancho = Convert.ToInt32(cadenaAuxiliar);
+
+            // 2.- Captar alto
+            Console.Write("Introduzca el alto del triangulo: ");
+            cadenaAuxiliar = Console.ReadLine();
+            alto = Convert.ToInt32(cadenaAuxiliar);
+
+            // PROCESO
+            superficieEntera = (ancho * alto) / 2;
+            superficieDecimal = (ancho * alto) / 2;
+            
+            // Conversion explicita de uno de los operando de la division empleando CASTING
+            superficieDecimal = (float)(ancho * alto) / 2;
+
+
+            // SALIDA
+            Console.WriteLine("El triangulo cuya Base es de " + ancho + " metros y altura es de " + alto + " metros ");
+            Console.WriteLine("Tiene una superficie de " + superficieEntera + " metros cuadrados");
+
+            // Version de salida PARAMETRIZADA
+            Console.WriteLine($"\n\n\tEl triangulo cuya Base es de {ancho} metros y altura es de {alto} metros ");
+            Console.WriteLine($"\tTiene una superficie de {superficieDecimal} metros cuadrados");
+
+        }
+    }
+}
---
+++ Proyectos Programacion/R3/R3E07_AlbertoDM/R3E07_AlbertoDM/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace R3E07_AlbertoDM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONSTANTES
+
+            // VARIABLES
+            int edad;
+            bool mayorEdad;
+            string cadenaAuxiliar;
+
+            // ENTRADA
+            Console.Write("Introduzca su edad: ");
+            cadenaAuxiliar = Console.ReadLine();
+            edad = Convert.ToByte(cadenaAuxiliar);
+
+            // PROCESO
+            mayorEdad = edad >= 18;
+
+            // SALIDA
+            Console.WriteLine("El usuario con " + edad + " años");
+            // .ToString().ToLower() es necesario para que salga "true" o "false" en minusculas
+            Console.WriteLine("Mayor de edad: " + mayorEdad.ToString().ToLower());
+
+        }
+    }
+}
